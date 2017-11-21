fbf6c098bef48c909166e1417702001eee2422d8
ih8mylyf <32474568+ih8mylyf@users.noreply.github.com>
2017-11-20 21:33:09 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <cstring>
 
+
 typedef unsigned char byte;   
 
 using namespace std;
@@ -15,106 +16,171 @@ using namespace std;
 const double frames_per_second = 30; 
 const int duration_in_seconds = 10;
 
-unsigned char frame[H][W][3];
 
-class object {
-	public:	
-	
- 		
+class Frame {
+public:
+	Frame(int w, int h);
+	~Frame();
+	void setPixel(int x, int y, byte r, byte g, byte b);
+	void clear();
+	bool isOutside(int x, int y) const;
+	void write(FILE * file) const;
+
+
+private:
+	byte * pixels;
+	int w;
+	int h;
+};
+
+
+class object {	
+	public:
+		void draw();
 	private:
-
+		byte * pixels;
+		int x;
+		int y;
+		int w;
+		int h;
+		int xs;
+		int ys;
 };
 
 class image : public object {
 	public: 
-		image(FILE * f, int x, int y, int s);
-		void load(char * filename);
-
-		void Right(double t);
-		void clamp(int * x, int * y);	void setPos(int x, int y);
+		image(int x, int y, int xs, int ys);
+		~image() { delete [] pixels; }
+		void * load(const char * filename, int w, int h);
+		void draw();
 	private:
+		byte * pixels;
+		int w;
+		int h;
 		int x;
 		int y;
-		int s;
-		FILE * f;
-		unsigned * char pixels[728][546][3];
+		int xs;
+		int ys;
 };
 
 
 class rect : public object {
 	public:
-		rect(int x, int y, int w, int h, double s, byte r, byte g, byte b);
+		rect(int x, int y, int w, int h, int xs, int ys, byte r, byte g, byte b);
+		void setPos(int x, int y);
 		void setColor(byte r, byte g, byte b);
-		void Down(double t);
- 		void Up(double t);
-		void Right(double t);
-		void Left(double t);
-		void DownRight(double t);
-		void DownLeft(double t);
-		void UpRight(double t);
-		void UpLeft(double t);
-		void clamp(int * x, int * y);	void setPos(int x, int y);
 		void setSize(int w, int h);
-		void setSpeed(int s);
-	private:		
-		int w;
-		int h;
-		double s;
+	private:
+		byte * pixels;
 		int x;
 		int y;
+		int w;
+		int h;
+		int xs;
+		int ys;	
 		byte r;
 		byte g;
 		byte b;
 };
 
-image::image(FILE f, int x, int y){
-	this->f=f;
+image::image(int x, int y, int xs, int ys){
 	this->x=x;
 	this->y=y;
-	this->s=s;
+	this->xs=xs;
+	this->ys=ys;
 }
 
-void image::load(){
-	
-	
-}
+void * image::load(const char * filename, int w, int h){
+	this->w = w;
+	this->h = h;
+	pixels = new byte[w * h * 3];
 
-void image::Right(double t){
-	int x0 = x+t*s;
-	int x1 = x0 + w;
-	int y0 = y;
-	int y1 = y0 + h;
-	int bpmY = 0;
-	int bpmX = 0;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = 
-			frame[y][x][1] = 
-			frame[y][x][2] = 
-		}
+	const char * cmd = 
+		"ffmpeg            "
+		"-i ball.png      "
+		"-f rawvideo       "
+		"-pix_fmt rgb24    "
+		"-vf scale=100x100 "
+		"-                 ";
+#ifdef _WIN32
+	FILE * pipe = _popen(cmd, "rb");
+#else
+	FILE * pipe = popen(cmd, "r");
+#endif
+	if (pipe == 0) {
+		std::cout << "error: " << strerror(errno) << std::endl;
+		exit(1);
 	}
+	fread(pixels, 3, w * h, pipe);
+
+	fflush(pipe);
+#ifdef _WIN32
+	_pclose(pipe);
+#else
+	pclose(pipe);
+#endif
 }
 
+Frame frame(720, 480);
 
-void image::clamp(int * x, int * y){
-	if (*x < 0) *x = 0; else if (*x >= W) *x = W - 1;
-	if (*y < 0) *y = 0; else if (*y >= H) *y = H - 1;
+void image::draw(){
+	for (int i = 0; i < w; ++i)
+		for (int j = 0; j < h; ++j){
+			byte r = pixels[j * w * 3 + i * 3 + 0];
+			byte g = pixels[j * w * 3 + i * 3 + 1];
+			byte b = pixels[j * w * 3 + i * 3 + 2];
+			frame.setPixel(x + i, y + j, r, g, b);
+		}
+	x+=xs;
+	y+=ys;
 }
 
+Frame::Frame(int w, int h)  {
+	this->w = w;
+	this->h = h;
+	pixels = new byte[w * h * 3];
+}
 
+Frame::~Frame() {
+	delete pixels;
+}
 
+void Frame::setPixel(int x, int y, byte r, byte g, byte b) {
+	pixels[y * w * 3 + x * 3 + 0] = r;
+	pixels[y * w * 3 + x * 3 + 1] = g;
+	pixels[y * w * 3 + x * 3 + 2] = b;
+}
 
-rect::rect(int x, int y, int w, int h, double s, byte r, byte g, byte b){
+void Frame::clear() {
+	memset(pixels, 0, h * w * 3);
+}
+
+bool Frame::isOutside(int x, int y) const {
+	return x < 0 || x >= w || y < 0 || y >= h;
+}
+
+void Frame::write(FILE * file) const {
+	fwrite(pixels, 3, w * h, file);
+}
+
+rect::rect(int x, int y, int w, int h, int xs, int xy, byte r, byte g, byte b){
 	this->x=x;
 	this->y=y;
 	this->w=w;
 	this->h=h;
-	this->s=s;
+	this->xs=xs;
+	this->ys=ys;
 	this->r=r;
 	this->g=g;
 	this->b=b;
+	pixels = new byte[w * h * 3];
+	for(int i = 0; i < w; ++i)
+		for(int j = 0; j < h; ++j){
+			pixels[j * w * 3 + i * 3 + 0]=r;
+			pixels[j * w * 3 + i * 3 + 1]=g;
+			pixels[j * w * 3 + i * 3 + 2]=b;
+		}
+
 }
 
 void rect::setPos(int x, int y){
@@ -133,152 +199,17 @@ void rect::setColor(byte r, byte g, byte b){
     this->b=b;
 }
 
-void rect::Down(double t){
-	int x0 = x;
-	int x1 = x0 + w;
-	int y0 = y+t*s;
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-void rect::Up(double t){
-	int x0 = x;
-	int x1 = x0 + w;
-	int y0 = y-(t*s);
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-void rect::Right(double t){
-	int x0 = x+t*s;
-	int x1 = x0 + w;
-	int y0 = y;
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-void rect::Left(double t){
-	int x0 = x-t*s;
-	int x1 = x0 + w;
-	int y0 = y;
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-void rect::DownRight(double t){
-	int x0 = x+t*s;
-	int x1 = x0 + w;
-	int y0 = y+t*s;
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-void rect::DownLeft(double t){
-	int x0 = x-t*s;
-	int x1 = x0 + w;
-	int y0 = y+t*s;
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-void rect::UpRight(double t){
-	int x0 = x+t*s;
-	int x1 = x0 + w;
-	int y0 = y-t*s;
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-void rect::UpLeft(double t){
-	int x0 = x-t*s;
-	int x1 = x0 + w;
-	int y0 = y-t*s;
-	int y1 = y0 + h;
-	clamp(&x0, &y0);
-	clamp(&x1, &y1);
-	for (int y = y0; y < y1; ++y) {
-		for (int x = x0; x < x1; ++x) {
-			frame[y][x][0] = r;
-			frame[y][x][1] = g;
-			frame[y][x][2] = b;
-		}
-	}
-}
-
-
-
-void rect::clamp(int * x, int * y){
-	if (*x < 0) *x = 0; else if (*x >= W) *x = W - 1;
-	if (*y < 0) *y = 0; else if (*y >= H) *y = H - 1;
-}
-
-void clear_frame() { memset(frame, 0, sizeof(frame)); }
 
 int main(int argc, char * argv[]) {
 	// Construct the ffmpeg command to run.
-	rect a(0, 0, 40, 10, 300, 0x00, 0xff, 0x00);
-	rect b(710, 460, 10, 20, 60, 0x00, 0x00, 0xff);
-	rect c(0, 30, 50, 5, 40, 0xff, 0x00, 0x00);
-	rect d(300, 90, 300, 300, 100, 0x00, 0xff, 0x00);
-	
-	image(0, 0, 80);
-	
+	rect a(0, 0, 40, 10, 2, 0, 0x00, 0xff, 0x00);
+	rect b(710, 460, 10, 20, 0, 2, 0x00, 0x00, 0xff);
+	image ball(0, 50, 5, 0);
+	ball.load("ball.png", 100, 100);	
+	image ball2(720, 180, -2, 0);
+	ball2.load("ball.png", 100, 100);
+	image ball3(0, 380, 2, -2);
+	ball3.load("ball.png", 100, 100);
 	const char * cmd = 
 		"ffmpeg              "
 		"-y                  "
@@ -293,10 +224,7 @@ int main(int argc, char * argv[]) {
 		"-an                 " // no audio
 		"-q:v 5              " // quality level; 1 <= q <= 32
 		"output.mp4          ";
-	const char * cmd2 = 
-		"ffmpeg -i car.jpg -f rawvideo -pix_fruit rgb24";
-	FILE * f = popen(cmd2, "r");
-	
+
 	// Run the ffmpeg command and get pipe to write into its standard input stream.
 	FILE * pipe = popen(cmd, "w");
 	if (pipe == 0) {
@@ -304,9 +232,21 @@ int main(int argc, char * argv[]) {
 		return 1;
 	}
 
-	// Write video frames into the pipe.
 	int num_frames = duration_in_seconds * frames_per_second;
-/*	for (int i = 0; i < num_frames/2 ; ++i) {
+
+	for (int i = 0; i < num_frames/2; ++i){
+		frame.clear();
+		ball.draw();
+		ball2.draw();
+		ball3.draw();
+//		a.draw();
+//		b.draw();
+		frame.write(pipe);
+	}
+	
+	
+	
+	/*	for (int i = 0; i < num_frames/2 ; ++i) {
 		double time_in_seconds = i / frames_per_second;
 		clear_frame();
 		a.Down(time_in_seconds);
@@ -317,7 +257,6 @@ int main(int argc, char * argv[]) {
 	}
 	d.setSize(5000, 5000);
 	fwrite(frame, 3, W * H, pipe);
-
 	a = rect(350, 230, 20, 20, 50, 0x00, 0xff, 0x00);
 	b= rect(340, 220, 40, 40, 50, 0x00, 0x00, 0xff);
 	double time;
@@ -334,14 +273,7 @@ int main(int argc, char * argv[]) {
 		b.Down(time);
 		fwrite(frame, 3, W * H, pipe);
 	}
-*/
-	for (int i = 0; i < num_frames ; ++i) {
-		double time = i / frames_per_second;
-		clear_frame();
-		pic.right(time);
-		fwrite(frame, 3, W * H, pipe);
-	}
-	
+*/	
 	
 	fflush(pipe);
 	pclose(pipe);
