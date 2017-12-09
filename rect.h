d85100289a7f811bd3d1c27ebfb33bb9ec18a397
ih8mylyf <32474568+ih8mylyf@users.noreply.github.com>
2017-12-09 12:22:29 -0800

    Update rect.h

--- rect.h
+++ rect.h
@@ -8,7 +8,7 @@ class rect : public object {
 	public:
 		rect(int x, int y, int w, int h, int xs, int ys, byte r, byte g, byte b);
 		void draw();
-		void advance(int time){x+=xs;y+=ys;}
+		void advance(int time){x+=xs;y+=ys;} //parameter time is not needed for this function, however is kept to match the Clock's advance function
 	private:
 		byte r;
 		byte g;
