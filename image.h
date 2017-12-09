ba2ea7103c05127ec64a65fd80bffb33a9d688f1
ih8mylyf <32474568+ih8mylyf@users.noreply.github.com>
2017-12-09 12:27:34 -0800

    Update image.h

--- image.h
+++ image.h
@@ -6,6 +6,6 @@ class image : public object {
 	public:
 		image(int x, int y, int xs, int ys);
 		void draw();
-		void advance(int time){x+=xs;y+=ys;}
+		void advance(int time){x+=xs;y+=ys;} //'time' not needed, but still put in place in order to match the Clock's advance function
 		void * load(const char * filename, int w, int h);
 };
